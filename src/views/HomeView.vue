154d576bea83d7756bd84c68dc786cdb8ca585ac
agonzalezs <agonzalezs@minsait.com>
2022-11-19 18:57:07 +0100

    First commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+// import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    Home
+    <div class="text-4xl">Hello 👋🏼</div>
+  </main>
+</template>
